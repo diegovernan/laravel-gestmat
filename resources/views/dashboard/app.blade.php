e946def61d273e84c7e316930968e6b85936c34c
Diego Vernan <diegovernan@gmail.com>
2020-01-29 13:14:43 -0300

    Atualizaçoes gerais

--- resources/views/dashboard/app.blade.php
+++ resources/views/dashboard/app.blade.php
@@ -67,8 +67,8 @@
         </div>
     </div>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.24.1/feather.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
     <script src="{{ asset('js/dashboard.js') }}"></script>
 </body>
 

eb1209244332712c9ec9fcbcf7c92375ffa17069
Diego Vernan <diegovernan@gmail.com>
2020-02-20 18:43:14 -0300

    Traduçao da verify-email

--- resources/views/emails/verify-email.blade.php
+++ resources/views/emails/verify-email.blade.php
@@ -3,10 +3,10 @@
 
 <h1>Olá!</h1>
 
-<p>Por favor, clique no botão abaixo para verificar seu e-mail.</p>
+<p>Por favor, clique no botão abaixo para confirmar seu e-mail.</p>
 
 @component('mail::button', ['url' => $url])
-Verify Email
+Confirmar e-mail
 @endcomponent
 
 Caso você não tenha criado uma conta, basta ignorar este esta mensagem.

38dc9f1140737e860469f14b1b9bd0ff68ca3e4b
Diego Vernan <diegovernan@gmail.com>
2021-07-26 18:35:06 -0300

    Ajustes na interface

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -2,23 +2,21 @@
 
 @section('content')
 
-<div class="container py-5">
-    <h4 class="text-center">Atualizar perfil</h4>
+<h3 class="py-3">Atualizar perfil</h3>
 
-    @include('alerts.messages')
+@include('alerts.messages')
 
-    <form method="post" action="{{ route('profile.update', $user->id) }}">
-        @csrf
-        @method('PATCH')
-        <div class="form-group">
-            <label for="Input1">Nome</label>
-            <input type="text" class="form-control" id="Input1" name="name" value="{{ old('name') ?? $user->name }}">
-        </div>
+<form method="post" action="{{ route('profile.update', $user->id) }}">
+    @csrf
+    @method('PATCH')
+    <div class="form-group">
+        <label for="Input1">Nome</label>
+        <input type="text" class="form-control" id="Input1" name="name" value="{{ old('name') ?? $user->name }}">
+    </div>
 
-        <div class="text-center">
-            <button type="submit" class="btn btn-primary">Salvar</button>
-        </div>
-    </form>
-</div>
+    <div class="text-center pb-5">
+        <button type="submit" class="btn btn-primary">Salvar</button>
+    </div>
+</form>
 
 @endsection
--- resources/views/users/security.blade.php
+++ resources/views/users/security.blade.php
@@ -2,33 +2,31 @@
 
 @section('content')
 
-<div class="container py-5">
-    <h4 class="text-center">Atualizar senha</h4>
+<h3 class="py-3">Atualizar senha</h3>
 
-    @include('alerts.messages')
+@include('alerts.messages')
 
-    <form method="post" action="{{ route('security.update', $user->id) }}">
-        @csrf
-        @method('PATCH')
-        <div class="form-group">
-            <label for="Input2">Senha atual</label>
-            <input type="password" class="form-control" id="Input2" name="old_password">
-        </div>
+<form method="post" action="{{ route('security.update', $user->id) }}">
+    @csrf
+    @method('PATCH')
+    <div class="form-group">
+        <label for="Input2">Senha atual</label>
+        <input type="password" class="form-control" id="Input2" name="old_password">
+    </div>
 
-        <div class="form-group">
-            <label for="Input3">Nova senha</label>
-            <input type="password" class="form-control" id="Input3" name="new_password">
-        </div>
+    <div class="form-group">
+        <label for="Input3">Nova senha</label>
+        <input type="password" class="form-control" id="Input3" name="new_password">
+    </div>
 
-        <div class="form-group">
-            <label for="Input4">Confirmação da nova senha</label>
-            <input type="password" class="form-control" id="Input4" name="password_confirm">
-        </div>
+    <div class="form-group">
+        <label for="Input4">Confirmação da nova senha</label>
+        <input type="password" class="form-control" id="Input4" name="password_confirm">
+    </div>
 
-        <div class="text-center">
-            <button type="submit" class="btn btn-primary">Salvar</button>
-        </div>
-    </form>
-</div>
+    <div class="text-center pb-5">
+        <button type="submit" class="btn btn-primary">Salvar</button>
+    </div>
+</form>
 
 @endsection

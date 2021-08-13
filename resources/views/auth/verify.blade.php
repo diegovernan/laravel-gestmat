b240fa367be8106bcb9b54001ab299767f663553
Diego Vernan <diegovernan@gmail.com>
2021-08-13 15:17:59 -0300

    ajuste em verify

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,13 +5,13 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card my-5">
-                <div class="card-header">Verifique o seu e-mail</div>
+                <div class="card-header text-center">Verifique o seu e-mail</div>
 
                 <div class="card-body">
                     @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            Um novo link de verificação foi enviado para o seu e-mail.
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        Um novo link de verificação foi enviado para o seu e-mail.
+                    </div>
                     @endif
 
                     Antes de prosseguir, por favor procure pelo link de verificação no seu e-mail.
